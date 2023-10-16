c0b68425b7f867f9f7d4848a7778ec423a35fe0f
Diasha Kar <109426754+missfreak13@users.noreply.github.com>
2023-10-17 02:26:42 +0530

    Update Q5_1070.py

--- Q5_1070.py
+++ Q5_1070.py
@@ -4,13 +4,13 @@
 #https://cses.fi/problemset/task/1070
 
 n=int(input())
+e=[]
+o=[]
 if n==1:
     print(1)
 elif n<=3:
     print("NO SOLUTION")
 else:
-    e=[]
-    o=[]
     for i in range(n, 0,-1):
         if i%2==0:
             e.append(i)
